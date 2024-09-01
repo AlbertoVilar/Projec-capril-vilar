a8402f7f407e8ee23e590f35ee849097d769d7ba
Alberto Vilar <albertovilar1@gmail.com>
2024-09-01 00:59:12 -0300

    Created packages, dependencies

---
+++ src/main/java/br/com/vilar/capril/SpringddApplication.java
@@ -0,0 +1,13 @@
+package br.com.vilar.capril;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringddApplication.class, args);
+	}
+
+}
